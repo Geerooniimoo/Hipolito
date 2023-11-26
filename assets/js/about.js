d3a57e9c90fd485bbea6a0db3722025e963384de
Geronimo Perez <geronimo.perez4@gmail.com>
2023-11-25 21:19:26 -0500

    worked on about page for an hour

---
+++ assets/js/about.js
@@ -0,0 +1,10 @@
+window.onclick = ({target}) => {
+    console.log(target);
+    if(target == whoWeAre) {
+        console.log('clicked');
+        aboutDiv2.classList.toggle('hide')
+    }
+    
+    return console.log('clicked');
+    console.log(id);
+}    
---
+++ tasks.md
@@ -0,0 +1,7 @@
+
+11/13/23
+- email
+- logo
+- images
+- about
+- similar to website
