931f9931c9ae5bca55ccd269c7e91b16dd281d9d
Geronimo Perez <geronimo.perez4@gmail.com>
2023-12-29 17:41:06 -0500

    fixing email delivery with Hipolito [1/2h]

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -5,15 +5,15 @@ submit.onclick = e => {
 
     Email.send({
         SecureToken: "984b5335-048f-484a-95e8-3584fc750e32",
-        // To: 'biondar@hispanicfemalenetworksolutions.com',
-        To: 'romero.hipolito@gmail.com',
+        To: 'biondar@hispanicfemalenetworksolutions.com',
+        // To: 'romero.hipolito@gmail.com',
         From: "Geronimo.Perez4@Gmail.com",
         Subject: "Potential client",
         Body: `
         <h3>Hi Mrs. Lalondriz,</h3>
         
         <p>
-        ${firstName.value} ${lastName.value} whose phone number is ${phone.value}, and is interested in ${products.value} filled out a form on the Hispanic Female Network Solutions website on ${new Date().toDateString()} at ${new Date().toLocaleTimeString()} with the following message:
+        ${firstName.value} ${lastName.value} whose phone number and email are ${phone.value}, ${email.value}, and is interested in ${products.value} filled out a form on the Hispanic Female Network Solutions website on ${new Date().toDateString()} at ${new Date().toLocaleTimeString()} with the following message:
         </p>
         <p>
         ${help.value}
