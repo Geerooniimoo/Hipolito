f164e2d326e936eabf50b85349fb85f00c5cb05d
Geronimo Perez <geronimo.perez4@gmail.com>
2024-01-10 12:05:02 -0500

    completed local testing successfully. [1/2h]

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -4,13 +4,10 @@ submit.onclick = e => {
     e.preventDefault();
 
     Email.send({
-        SecureToken: "984b5335-048f-484a-95e8-3584fc750e32",
-        // To: 'Geronimo.Perez4@Gmail.com',
-        // To:'ggandalex@hotmail.com',
-        To: 'biondar@hispanicfemalenetworksolutions.com',
-        // To: 'romero.hipolito@gmail.com',
-        From: "Geronimo.Perez4@Gmail.com",
-        // From: "ggandalex@hotmail.com",
+        SecureToken: "32f50c8c-0771-45f1-960b-b265b14d42e1",
+        To: 'Geronimo.Perez4@gmail.com',
+        // To: 'biondar@hispanicfemalenetworksolutions.com',
+        From: "hifenetllc@gmail.com",
         Subject: "Potential client",
         Body: `
         <h3>Hi Mrs. Lalondriz,</h3>
@@ -25,26 +22,15 @@ submit.onclick = e => {
         Sincerely,
         </p>
         <p>
-        Geronimo P.
+        Hifenet-Bot
         </p>`
     }).then(
         message => {
             alert(message);
             window.location.reload();
-        });
+        }
 
-    // Email.send({
-    //     SecureToken: "e50833d9-84e9-46a8-8fdd-32b2d0817995",
-    //     To: 'ggandalex@hotmail.com',
-    //     From: "hifenetllc@gmail.com",
-    //     Subject: "test",
-    //     Body: "test test 1 2 3"
-    // }).then(
-    //     message => {
-    //         alert(message);
-    //         window.location.reload();
-    //     }
+    );
 
-    // );
 };
 
